b0f3cb952aceb796be58cbf7412ae3ce2d8ea4cb
epsonagunda <epsonagunda182@gmail.com>
2024-06-25 23:57:16 -0700

    user

--- models/user.py
+++ models/user.py
@@ -1,34 +1,11 @@
 #!/usr/bin/python3
-"""A class User module"""
-import os
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-
-from models.base_model import BaseModel, Base
+"""defines a class User module"""
+from models.base_model import BaseModel
 
 
-class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-    email = Column(
-        String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    password = Column(
-        String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    first_name = Column(
-        String(128), nullable=True
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    last_name = Column(
-        String(128), nullable=True
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    places = relationship(
-        'Place',
-        cascade="all, delete, delete-orphan",
-        backref='user'
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
-    reviews = relationship(
-        'Review',
-        cascade="all, delete, delete-orphan",
-        backref='user'
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
+class User(BaseModel):
+    """user by various attributes"""
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''

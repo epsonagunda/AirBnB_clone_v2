c581c5d01267efaa8e26c68f813be541c15a309b
epsonagunda <epsonagunda182@gmail.com>
2024-07-26 05:32:57 -0700

    web flask

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+script that starts flask web application
+"""
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index():
+    """return Hello HBNB!"""
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """return HBNB"""
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun():
+    """display "c", followed by the value of the text varriable"""
+    return 'c' + text.replace('_', '')
+
+@app.route('/python',strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text='is cool'):
+    """ display "python", followed by the value of the text varriable"""
+    return 'python ' + text.replce('_','')
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def imanumber(n):
+    """display "n is a nmber" only if n is an integer"""
+    return "{:d} is a number ".format(n)
+
+if __name__ == '__main__'
+app.run(host='0.0.0.0', port='5000')
+
